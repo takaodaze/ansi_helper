35e058f7e116f2086b9ee4b9ed6462c5036883be
takaodaze <gin2798@gmail.com>
2023-12-12 03:58:22 +0900

    バンドラとか準備

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { CharStyle, Color } from './ANSICode'
-import { ANSIEscSeqHelper } from './ANSIEscSeqHelper'
-import { CharRenderer } from './CharRenderer'
-
-async function main (): Promise<void> {
-  const charRederer = new CharRenderer()
-  charRederer.writeLine('aaa')
-  charRederer.writeLine('aaa')
-  charRederer.addIndent()
-  charRederer.writeLine('aaa')
-  charRederer.addIndent()
-  ANSIEscSeqHelper.changeCharStyle(new CharStyle('underline'))
-  ANSIEscSeqHelper.changeColor(new Color('green'))
-  charRederer.writeLine('aaa')
-  charRederer.reduceIndent()
-  ANSIEscSeqHelper.changeColor(new Color('red'))
-  ANSIEscSeqHelper.changeCharStyle(new CharStyle('bold'))
-  charRederer.writeLine('aaa')
-  charRederer.reduceIndent()
-  ANSIEscSeqHelper.resetCharAllStyle()
-  charRederer.writeLine('aaa')
-}
-
-void (async () => {
-  await main()
-})()
+export { Color, CharStyle } from './ANSICode'
+export { ANSIEscSeqHelper } from './ANSIEscSeqHelper'
+export { CharRenderer } from './CharRenderer'
+export { Progress } from './Progress'
+export { PromptHelper } from './PromptHelper'
